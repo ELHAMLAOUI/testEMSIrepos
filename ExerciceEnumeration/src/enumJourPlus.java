98585a81d28d56b73c7826a424261209356292a8
Rabat IT Center Autumn School <mahmoud.elhamlaoui@gmail.com>
2020-11-21 11:42:56 +0100

    correction value enum

--- ExerciceEnumeration/src/enumJourPlus.java
+++ ExerciceEnumeration/src/enumJourPlus.java
@@ -1,5 +1,5 @@
 public enum enumJourPlus {
-	LUNDI(8), MARDI(8), MERCREDI(4), JEUDI(8), VENDREDI(7), SAMEDI(0), DIMANCHE(0);
+	LUNDI(8), MARDI(9), MERCREDI(4), JEUDI(8), VENDREDI(7), SAMEDI(0), DIMANCHE(0);
 	private int nbHeures;
 	enumJourPlus(int nb) {nbHeures = nb;}
 	public int getNbHeures() {return nbHeures;}
@@ -8,6 +8,7 @@ public enum enumJourPlus {
 			case SAMEDI : return "cinema";
 			case DIMANCHE : return "dormir";
 			default : return "travailler";
+			
 		}
  	}
 }
